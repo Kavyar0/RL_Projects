df77db71262289e6c53625263e41a22d75628a91
Kavyar0 <75579567+Kavyar0@users.noreply.github.com>
2020-12-06 23:43:50 -0800

    Update CartPole_RandomReward.py

--- CartPole_RandomReward.py
+++ CartPole_RandomReward.py
@@ -2,7 +2,6 @@ import gym
 import numpy as np
 
 parameters = np.random.rand(4) * 2-1
-# action = 0 if np.matmul(parameters, observation) < 0 else 1
 
 def run_episode(env, parameters):
     observation = env.reset()
